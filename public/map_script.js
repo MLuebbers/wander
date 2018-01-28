057a76531709957a1756ddf6bebc0f41fc3c3781
Jimmy Niu <chenenniu@gmail.com>
2018-01-28 10:49:59 -0500

    WE ADD THE POINTS!

--- public/map_script.js
+++ public/map_script.js
@@ -145,7 +145,7 @@ function drawPoints(){
             // var lat2 = 41.8267760;
             // var lon2 = -71.40441821;
             var lat2 = parseFloat(msg[x].longitude);
-            var lon2 = parseFloat(msg[41.8267760, -71.4044182].latitude);
+            var lon2 = parseFloat(msg[x].latitude);
             var words = msg[x].words;
             // console.log(lat);
             // console.log(lon);
