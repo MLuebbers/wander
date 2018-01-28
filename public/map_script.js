bdc0c2c6e48bbdf6141956edec183471689a9341
Jimmy Niu <chenenniu@gmail.com>
2018-01-28 10:42:10 -0500

    working?

--- public/map_script.js
+++ public/map_script.js
@@ -142,8 +142,8 @@ function drawPoints(){
 
         alert("Data Saved: " + msg)
         for(x = 0; x < msg.length; x++){
-            var lat2 = parseFloat(msg[x].latitude);
-            var lon2 = parseFloat(msg[x].longitude);
+            var lat2 = 41.8267760; //parseFloat(msg[41.8267760, -71.4044182].latitude);
+            var lon2 = -71.4044182l;// parseFloat(msg[x].longitude);
             var words = msg[x].words;
             // console.log(lat);
             // console.log(lon);
