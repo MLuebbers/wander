835b6e3c867d81063c1d38ca814deaaefd1019da
jtishler <jenna@tishler.net>
2018-01-28 04:18:20 -0500

    move db

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const connection = mysql.createConnection({
     password: 'password'
 });
 
+connection.query("CREATE DATABASE wander", function (err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log("Database created");
+    });
+
 connection.connect(function(err) {
     if(err){
         console.error('error connecting: ' + err.stack);
@@ -18,12 +25,12 @@ connection.connect(function(err) {
 
     console.log('connected as id ' + connection.threadId);
 
-    connection.query("CREATE DATABASE wander", function (err, result) {
-        if (err) {
-            throw err;
-        }
-        console.log("Database created");
-    });
+    // connection.query("CREATE DATABASE wander", function (err, result) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     console.log("Database created");
+    // });
 
     var sql = "CREATE TABLE points (longitude FLOAT, latitude FLOAT, words VARCHAR(140), free BOOLEAN)";
     connection.query(sql, function (err, result) {
